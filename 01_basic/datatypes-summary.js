2fbc615f74252287bf1f6f62dde612befaa44084
AMIT KUMAR RAI <118451363+amitKrai22@users.noreply.github.com>
2024-12-24 13:21:25 +0000

    datatypes summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// 7 types : String, Number, boolean, Symbol, null, undefined, BigInt
+
+const score = 110
+const scorevalue = 110.9
+
+const isLoggedIn = true
+const outsideTemp = null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+// Reference (Non - Premitive)
+// Array, Object, Fucntions
+
+const hero = ['atom','minirals', 'hydro']
+let obj = {
+    'name': 'atom',
+    'weight': 12,
+}
+
+const fun = function(){
+    console.log("hello world");
+    
+}
