f5822088cb7b6a3f992ea1101cc9f61bf57614f0
AMIT KUMAR RAI <118451363+amitKrai22@users.noreply.github.com>
2024-12-24 14:30:15 +0000

    string discussion

---
+++ 01_basic/05_string.js
@@ -0,0 +1,35 @@
+let reponame = 'atom'
+let repocount = 40
+
+// console.log(reponame + repocount + " value");
+
+console.log(`my repo name is ${reponame} and number of is ${repocount}`);
+// console.log(reponame[2]);
+
+const gameName = new String('warhunt')
+
+// console.log(gameName[1]);
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('u'));
+
+const newString = gameName.substring(0, 5)
+// console.log(newString);
+
+const anotherString = gameName.slice(-6, 4)
+// console.log(anotherString);
+
+const newStringOne = ' atom  '
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://periodic.com/periodic%50element"
+// console.log(url.replace("%50", "-"));
+
+// console.log(url.includes('atom'));
+
+// console.log(url.split('/', 3));
+
+
+
