d9cdc702d77e9b424a3a13e041dc950b2d44e663
AMIT KUMAR RAI <118451363+amitKrai22@users.noreply.github.com>
2024-12-24 12:25:08 +0000

    conversion on datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score =  "amot"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// '33' => 33
+// '33abc' => NAN
+// false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "atmo" => true
+
+let rollNo = 33
+
+let stringNumber = String(rollNo)
+console.log(stringNumber);
+
+
+
+
