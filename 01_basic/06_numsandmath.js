690dce637f6c668fd2117b737a788ed903559be7
AMIT KUMAR RAI <118451363+amitKrai22@users.noreply.github.com>
2024-12-24 15:00:32 +0000

    math and nums

---
+++ 01_basic/06_numsandmath.js
@@ -0,0 +1,34 @@
+const num = 100
+
+const balance = new Number(110)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+
+const localNumber = 123.8934
+// console.log(localNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ******************Math********************** //
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.2234));
+// console.log(Math.floor(4.2));
+// console.log(Math.ceil(4.9));
+// console.log(Math.max(3,6,2,8));
+// console.log(Math.min(9,2,1,4));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()) * (max - min + 1) + min);
+
+
+
+
