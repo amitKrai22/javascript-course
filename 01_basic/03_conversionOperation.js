c228b6f75ade6d7aab51e9766a813eccdd7ae5c6
AMIT KUMAR RAI <118451363+amitKrai22@users.noreply.github.com>
2024-12-24 12:50:57 +0000

    discussion on operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score =  "amot"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -25,7 +25,37 @@ console.log(booleanIsLoggedIn);
 let rollNo = 33
 
 let stringNumber = String(rollNo)
-console.log(stringNumber);
+// console.log(stringNumber);
+
+// ****************Operations********************//
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " atom"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1 + "2" + "3");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true); // 1
+// console.log(+"");   // 0
+
+let counter = 100
+let y = counter++;
+let z = ++counter
+console.log(counter, y, z);
+
+
+
 
 
 
