389748aba46cbdc4108f0c56e00c8f62f964c1dd
AMIT KUMAR RAI <118451363+amitKrai22@users.noreply.github.com>
2024-12-26 14:20:35 +0000

    discussion on control_flow

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -1,7 +1,19 @@
 // if
 
-isLoggedin = true
+const isLoggedin = true
+const temperature = 41
 if (isLoggedin){
-    console.log(`executed`);
+    // console.log(`executed`);
     
 }
+if (temperature < 50){
+    console.log(`The current temperature is ${temperature}`);
+    
+}
+else{
+    console.log(`Temperature is greater than 50`);
+    
+}
+
+const balance = 1000
+if (balance > 500) console.log("sufficent balance");
