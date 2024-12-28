6e5527f94584b5133860c59743504535df4627ed
AMIT KUMAR RAI <118451363+amitKrai22@users.noreply.github.com>
2024-12-28 12:44:00 +0000

    learn through project

---
+++ 07_projects/01_color-changer/script.js
@@ -0,0 +1,6 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+console.log(buttons);
+buttons.forEach((button) => {
+  console.log(button);
+});
